dff3d90fe9c4fdce91742a73e3ce62727147d928
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-12 14:03:54 +0200

    App Component: Title change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent {
     title = 'everesstTempoSyncUI';
 
     constructor() {
-        this.title = 'Spring Boot - Angular Application';
+        this.title = 'EveresstTempoSyncUI';
     }
 }
