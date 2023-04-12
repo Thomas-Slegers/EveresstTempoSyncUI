e92263740cdcb93720f5535143f7cbbfcb2877d7
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-12 13:44:13 +0200

    Refactoring: Refactoring of the file structure

--- src/app/service/error-entity.service.ts
+++ src/app/service/error-entity.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {ErrorEntity} from "./error-entity";
+import {ErrorEntity} from "../model/error-entity";
 
 @Injectable({
   providedIn: 'root'
