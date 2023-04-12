45fc3fc432a097afec1d1a8f76737ef8fba93b78
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-12 13:55:24 +0200

    Application Module: Add new components & services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ErrorEntityListComponent} from "./error-entity-list/error-entity-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'errors', component: ErrorEntityListComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ErrorEntityListComponent} from './error-entity-list/error-entity-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ErrorEntityService} from "./service/error-entity.service";
 
 @NgModule({
     declarations: [
@@ -12,9 +14,10 @@ import {ErrorEntityListComponent} from './error-entity-list/error-entity-list.co
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule
     ],
-    providers: [],
+    providers: [ErrorEntityService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
