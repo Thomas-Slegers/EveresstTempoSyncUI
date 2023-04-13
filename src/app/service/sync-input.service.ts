6b39b7de87d19ed1d488b0a0678cb8673444daa1
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-13 15:01:54 +0200

    Sync Input Service: Connect with backend with post request.

--- src/app/service/sync-input.service.ts
+++ src/app/service/sync-input.service.ts
@@ -1,14 +1,18 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {SyncInputEntity} from "../model/sync-input-entity";
 
 @Injectable({
     providedIn: 'root'
 })
 export class SyncInputService {
+    private syncUrl: string;
 
-    constructor() {
+    constructor(private http: HttpClient) {
+        this.syncUrl = 'http://localhost:8080/sync-input';
     }
 
-    public send() {
-        return null;
+    public startSync(syncInputEntity: SyncInputEntity) {
+        return this.http.post<SyncInputEntity>(this.syncUrl, syncInputEntity);
     }
 }
