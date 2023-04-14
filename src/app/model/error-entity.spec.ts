07103e12bac0f95a5f3aee8a43fdc4fbc8d8d6cd
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-14 14:48:14 +0200

    ErrorEntity: Removed files

--- src/app/model/error-entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorEntity } from './error-entity';
-
-describe('ErrorEntity', () => {
-  it('should create an instance', () => {
-    expect(new ErrorEntity()).toBeTruthy();
-  });
-});
--- src/app/model/error-entity.ts
+++
@@ -1,8 +0,0 @@
-export class ErrorEntity {
-    id: string
-    date: string
-    employee: string
-    errorCode: string
-    description: string
-    solution: string
-}
