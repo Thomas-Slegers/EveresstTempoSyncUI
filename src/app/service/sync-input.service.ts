1bd49108f216591f130cc7d38d23e538b9e32d00
Thomas-Slegers <Thomsleg@hotmail.com>
2023-04-14 10:28:43 +0200

    Sync Input Service: Adjusted start sync method and sync url

--- src/app/service/sync-input.service.ts
+++ src/app/service/sync-input.service.ts
@@ -1,18 +1,19 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {SyncInputEntity} from "../model/sync-input-entity";
+import {Observable} from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class SyncInputService {
-    private syncUrl: string;
+    private readonly syncUrl: string;
 
     constructor(private http: HttpClient) {
-        this.syncUrl = 'http://localhost:8080/sync-input';
+        this.syncUrl = 'http://localhost:8080/input';
     }
 
-    public startSync(syncInputEntity: SyncInputEntity) {
+    public startSync(syncInputEntity: SyncInputEntity): Observable<SyncInputEntity> {
         return this.http.post<SyncInputEntity>(this.syncUrl, syncInputEntity);
     }
 }
