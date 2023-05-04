688bf14c62fe2593286320bf94d327cf0a35cfca
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-04 16:10:51 +0200

    app module: SyncWeekEntityOverview Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SyncInputService} from "./service/sync-input.service";
 import {SearchFilterPipe} from "./sync-entity-list/search-filter-pipe";
 import {ClipboardModule} from "@angular/cdk/clipboard";
+import { SyncWeekEntityOverviewComponent } from './sync-week-entity-overview/sync-week-entity-overview.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        SearchFilterPipe,
         SyncEntityListComponent,
         SyncInputFormComponent,
-        SearchFilterPipe
+        SyncWeekEntityOverviewComponent
     ],
     imports: [
         BrowserModule,
