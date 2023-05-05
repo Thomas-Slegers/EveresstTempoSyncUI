64b2c4247e546fc065ef3a19483134d1eaa8a37a
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-05 14:52:15 +0200

    SyncEntityList Adjusted: List changed to overview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {SyncEntityListComponent} from "./sync-entity-list/sync-entity-list.component";
+import {SyncEntityOverviewComponent} from "./sync-entity-overview/sync-entity-overview.component";
 import {SyncInputFormComponent} from "./sync-input-form/sync-input-form.component";
 import {SyncWeekOverviewComponent} from "./sync-week-overview/sync-week-overview.component";
 
 const routes: Routes = [
     {path: 'input', component: SyncInputFormComponent},
-    {path: 'sync/:syncTableUUID', component: SyncEntityListComponent},
+    {path: 'sync/:syncTableUUID', component: SyncEntityOverviewComponent},
     {path: 'sync/:syncTableUUID/:resourceId/:date', component: SyncWeekOverviewComponent},
     {path: '**', redirectTo: '/'}
 ];
