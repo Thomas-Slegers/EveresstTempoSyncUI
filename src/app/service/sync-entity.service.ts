397c81607fd4cf5d4efec3139818f8412921e0be
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-15 16:04:25 +0200

    Sync Entity Service httpclient module added

--- src/app/service/sync-entity.service.ts
+++ src/app/service/sync-entity.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {SyncEntity} from "../model/sync-entity";
 import {Environment} from "../environment";
