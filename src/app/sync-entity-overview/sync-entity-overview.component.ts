b7b7131e3ac3605516e94cc1f98a433313acb540
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-16 13:36:50 +0200

    sync-entity-overview.component: filter on visibility and checkbox method added

--- src/app/sync-entity-overview/sync-entity-overview.component.ts
+++ src/app/sync-entity-overview/sync-entity-overview.component.ts
@@ -6,11 +6,14 @@ import {SyncEntityService} from "../service/sync-entity.service";
 
 @Component({
     selector: 'app-sync-entity-list',
-    templateUrl: './sync-entity-overview.component.html'
+    templateUrl: './sync-entity-overview.component.html',
+    styleUrls: ['./sync-entity-overview.component.css']
 })
 export class SyncEntityOverviewComponent implements OnInit {
     filter: string;
     syncEntities: SyncEntity[];
+    filteredEntities: SyncEntity[]
+    syncVisibility = true
 
     constructor(private route: ActivatedRoute, private clipboard: Clipboard, private router: Router, private syncEntityService: SyncEntityService) {
     }
@@ -21,7 +24,7 @@ export class SyncEntityOverviewComponent implements OnInit {
             this.syncEntityService.findBySyncTableUUID(syncTableUUID).subscribe({
                 next: (response) => {
                     this.syncEntities = response;
-                    this.syncEntities.sort((firstElement, secondElement) => firstElement.employeeName.localeCompare(secondElement.employeeName) || firstElement.startDate.localeCompare(secondElement.startDate));
+                    this.filterVisibilitySyncEntitiesAndSort()
                 },
                 error: (error) => {
                     console.log(error);
@@ -53,5 +56,19 @@ export class SyncEntityOverviewComponent implements OnInit {
     getOverviewWeek(syncEntity: SyncEntity) {
         let syncTableUUID = this.route.snapshot.paramMap.get("syncTableUUID")
         this.router.navigate(['./sync/' + syncTableUUID + '/' + syncEntity.resourceId + '/' + syncEntity.startDate])
+    }
+
+    changeSyncVisibility(event) {
+        this.syncVisibility = event.target.checked;
+        this.filterVisibilitySyncEntitiesAndSort();
+    }
+
+    filterVisibilitySyncEntitiesAndSort() {
+        if (!this.syncVisibility) {
+            this.filteredEntities = this.syncEntities.filter(syncEntity => !syncEntity.message.toLowerCase().includes("no sync action needed for"))
+        } else {
+            this.filteredEntities = this.syncEntities;
+        }
+        this.filteredEntities.sort((firstElement, secondElement) => firstElement.employeeName.localeCompare(secondElement.employeeName) || firstElement.startDate.localeCompare(secondElement.startDate));
     }
 }
