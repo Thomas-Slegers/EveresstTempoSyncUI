288fb589b1d88a407d9eba6f8021153da0c1cba7
Thomas-Slegers <Thomsleg@hotmail.com>
2023-05-16 13:32:48 +0200

    Modules: MDBootstrapModule added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {BrowserModule} from '@angular/platform-browser';
 import {ClipboardModule} from "@angular/cdk/clipboard";
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserModule} from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {MDBBootstrapModule} from "angular-bootstrap-md";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {SyncEntityService} from "./service/sync-entity.service";
+import {SyncInputService} from "./service/sync-input.service";
 import {SearchFilterPipe} from "./sync-entity-overview/search-filter-pipe";
 import {SyncEntityOverviewComponent} from './sync-entity-overview/sync-entity-overview.component';
 import {SyncInputFormComponent} from './sync-input-form/sync-input-form.component';
-import {SyncInputService} from "./service/sync-input.service";
 import {SyncWeekOverviewComponent} from './sync-week-overview/sync-week-overview.component';
 
 @NgModule({
@@ -27,6 +28,7 @@ import {SyncWeekOverviewComponent} from './sync-week-overview/sync-week-overview
         ClipboardModule,
         FormsModule,
         HttpClientModule,
+        MDBBootstrapModule,
         NgbModule,
         ReactiveFormsModule,
     ],
